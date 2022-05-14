1153a2c35c2858b4be847e574147e37df888af17
Sérgio Moura <smouracalmon@gmail.com>
2022-05-14 01:41:16 -0300

    Adicionando acceleração como atributo das dash pills

--- game/front/src/DashPill.ts
+++ game/front/src/DashPill.ts
@@ -1,7 +1,8 @@
 export default class DashPill {
     constructor(
         private _duration: number,
-        private _speed: number
+        private _speed: number,
+        private _acceleration: number
     ){}
     public get speed(): number {
         return this._speed;
@@ -9,5 +10,10 @@ export default class DashPill {
     public get duration(): number {
         return this._duration;
     }
+    
+    public get acceleration() : number {
+        return this._acceleration;
+    }
+    
     
 }
