e676c209050580274c867a33245e21699313d3de
Sérgio Moura <smouracalmon@gmail.com>
2022-05-19 03:52:36 -0300

    Acrescido o teste para DashPill

--- game/front/src/game-components/DashPill/DashPill.ts
+++ game/front/src/game-components/DashPill/DashPill.ts
@@ -1,9 +1,11 @@
 export default class DashPill {
+
     constructor(
         private _duration: number,
         private _speed: number,
         private _acceleration: number
     ){}
+    
     public get speed(): number {
         return this._speed;
     }
@@ -14,6 +16,5 @@ export default class DashPill {
     public get acceleration() : number {
         return this._acceleration;
     }
-    
     
 }
