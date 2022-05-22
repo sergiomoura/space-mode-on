320eef48700922c17da057d47c38293a233b40b2
Sérgio Moura <smouracalmon@gmail.com>
2022-05-22 03:54:32 -0300

    Mantendo bots parados para facilitar testes

--- game/front/src/game-components/Bot/Bot.ts
+++ game/front/src/game-components/Bot/Bot.ts
@@ -13,12 +13,12 @@ export default class Bot extends Player{
     private move() {
         requestAnimationFrame(() => this.move());
 
-        let random = (max: number) => {
-            return Math.round(2 * max * (Math.random() - 0.5));
-        }
+        // let random = (max: number) => {
+        //     return Math.round(2 * max * (Math.random() - 0.5));
+        // }
 
-        this._ship.pointTo(random(100), random(100));
-        this._ship.startMovingForward();
+        // this._ship.pointTo(random(100), random(100));
+        // this._ship.startMovingForward();
     }
 
 }
