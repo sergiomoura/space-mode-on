acc526fa5ad41a1b8f0cab2f1e627ebc76088fcb
Sérgio Moura <smouracalmon@gmail.com>
2022-06-16 05:51:13 -0300

    Melhorando controles mobile para multi touch

--- front/src/controls/MobileShipControls.ts
+++ front/src/controls/MobileShipControls.ts
@@ -2,26 +2,52 @@ import Ship from "../game-components/Ship/Ship";
 
 class MobileShipControls {
 
-    private mobileScreen:HTMLDivElement;
-    private btShooter: HTMLButtonElement;
-    private btDasher: HTMLButtonElement;
-    private btJoystick: HTMLButtonElement;
+    private _mobileScreen:HTMLDivElement;
+    private _btShooter: HTMLButtonElement;
+    private _btDasher: HTMLButtonElement;
+    private _btJoystick: HTMLButtonElement;
 
     constructor(private ship:Ship){
         
-        this.mobileScreen = <HTMLDivElement>document.getElementById('mobile-controls');
-        this.btShooter = <HTMLButtonElement>document.getElementById('shooter');
-        this.btDasher = <HTMLButtonElement>document.getElementById('dasher');
-        this.btJoystick = <HTMLButtonElement>document.getElementById('joystick');
+        this._mobileScreen = <HTMLDivElement>document.getElementById('mobile-controls');
+        this._btShooter = <HTMLButtonElement>document.getElementById('shooter');
+        this._btDasher = <HTMLButtonElement>document.getElementById('dasher');
+        this._btJoystick = <HTMLButtonElement>document.getElementById('joystick');
 
         this.associateEvents();
     
     }
 
     private associateEvents(){
-        this.btShooter.addEventListener('click', ()=>{this.ship.shoot()});
-        this.btDasher.addEventListener('click', ()=>{this.ship.dash()});
+
+        // Associando eventos ao botão de tiro
+        this._btShooter.addEventListener(
+            'touchstart',
+            (evt)=>{
+                evt.stopPropagation();
+                this.ship.shoot();
+            }
+        );
+
+        // Associando eventos ao botão de dash
+        this._btDasher.addEventListener(
+            'touchend',
+            evt=>{
+                evt.stopPropagation();
+                this.ship.dash()
+            }
+        );
+
+        // this._mobileScreen.addEventListener('touchstart', evt=>{evt.stopPropagation();console.log(evt)}, false)
+        // this._mobileScreen.addEventListener('touchmove', evt=>console.log(evt), false)
+        // this._mobileScreen.addEventListener('touchend', evt=>console.log(evt), false)
     }
+
+    private shoot(){
+
+    }
+
+    
 
 }
 
