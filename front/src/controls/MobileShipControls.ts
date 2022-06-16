38b5400c3b65845138123aa730a3ac2658b297b7
Sérgio Moura <smouracalmon@gmail.com>
2022-06-16 07:10:46 -0300

    Movendo frente, tras, esq, dir!

--- front/src/controls/MobileShipControls.ts
+++ front/src/controls/MobileShipControls.ts
@@ -1,5 +1,9 @@
 import Ship from "../game-components/Ship/Ship";
 
+const frac = Math.PI/8;
+const MaxTanDireita = Math.tan(3*frac);
+const MinTanDireita = -Math.tan(3*frac);
+
 class MobileShipControls {
 
     private _mobileScreen:HTMLDivElement;
@@ -7,7 +11,7 @@ class MobileShipControls {
     private _btDasher: HTMLButtonElement;
     private _btJoystick: HTMLButtonElement;
 
-    constructor(private ship:Ship){
+    constructor(private _ship:Ship){
         
         this._mobileScreen = <HTMLDivElement>document.getElementById('mobile-controls');
         this._btShooter = <HTMLButtonElement>document.getElementById('shooter');
@@ -25,7 +29,7 @@ class MobileShipControls {
             'touchstart',
             (evt)=>{
                 evt.stopPropagation();
-                this.ship.shoot();
+                this._ship.shoot();
             }
         );
 
@@ -34,12 +38,52 @@ class MobileShipControls {
             'touchend',
             evt=>{
                 evt.stopPropagation();
-                this.ship.dash()
+                this._ship.dash()
             }
         );
+        
+        // Associando eventos ao joystick
+        this._btJoystick.addEventListener(
+            'touchmove',
+            evt => {
+                evt.cancelBubble = true;
+
+                let x = evt.changedTouches[0].clientX - this._btJoystick.offsetLeft - this._btJoystick.offsetWidth/2 + 1;
+                let y = evt.changedTouches[0].clientY - this._btJoystick.offsetTop - this._btJoystick.offsetHeight/2 + 1;
+                let ratio = y/x;
+                
+                if(x > 0 && ratio < MaxTanDireita && ratio>MinTanDireita){
+                    this._ship.startMovingRight();
+                } else {
+                    this._ship.stopMovingRight();
+                }
+
+                if(x < 0 && ratio < MaxTanDireita && ratio>MinTanDireita){
+                    this._ship.startMovingLeft();
+                } else {
+                    this._ship.stopMovingLeft();
+                }
+
+                ratio = x/-y;
+
+                if(y < 0 && ratio < MaxTanDireita && ratio>MinTanDireita){
+                    this._ship.startMovingForward();
+                } else {
+                    this._ship.stopMovingForward();
+                }
+
+                if(y > 0 && ratio < MaxTanDireita && ratio>MinTanDireita){
+                    this._ship.startMovingBackwards();
+                } else {
+                    this._ship.stopMovingBackwards();
+                }
+
+
+            }
+        )
 
         // this._mobileScreen.addEventListener('touchstart', evt=>{evt.stopPropagation();console.log(evt)}, false)
-        // this._mobileScreen.addEventListener('touchmove', evt=>console.log(evt), false)
+        //this._mobileScreen.addEventListener('touchmove', evt=>console.log(evt))
         // this._mobileScreen.addEventListener('touchend', evt=>console.log(evt), false)
     }
 
